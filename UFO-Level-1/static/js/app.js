6fd352389253d4c8a8eeb01065ac6fc6c9231042
Deep Patel <deeppatel@live.com>
2020-09-21 17:29:58 -0400

    Update app.js

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,17 @@
-// from data.js
-var tableData = data;
+  // from data.js
+  var tableData = data;
 
-// YOUR CODE HERE!
+  // Get a reference to the table body
+  var tbody = d3.select("tbody");
+  
+  // Console.log the weather data from data.js
+  console.log(data);
+  
+  tableData.forEach((UFO_Info) => {
+    var row = tbody.append("tr");
+    Object.entries(UFO_Info).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
